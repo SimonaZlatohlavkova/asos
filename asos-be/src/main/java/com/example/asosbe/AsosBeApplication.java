a5ddb848bd0c349d2b90a214a93380ff56b028b1
simona <xzlatohlavkova@stuba.sk>
2024-09-26 19:21:03 +0200

    Initialization

---
+++ asos-be/src/main/java/com/example/asosbe/AsosBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.asosbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsosBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsosBeApplication.class, args);
+    }
+
+}
---
+++ asos-be/src/test/java/com/example/asosbe/AsosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asosbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsosBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
